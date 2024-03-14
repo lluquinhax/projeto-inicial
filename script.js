d672f586fac997664aaeaab1fee4c424c4e11a09
lluquinhax <lucasfran1209@gmail.com>
2024-03-14 09:48:58 -0300

    mensagem contém nome

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click",alertar,false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
-    alert("voce clicou no botao!!!" + event);
+    alert("voce clicou no botao!!!" + nome.value);
 }
