98f3d808d86b0bc7bb92940441bd9f7563f57eef
lluquinhax <lucasfran1209@gmail.com>
2024-03-19 08:23:54 -0300

    formulario

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click",alertar,false);
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
 function alertar(event){
-    alert("voce clicou no botao!!!" + nome.value);
+    //alert("voce clicou no botao!!!" + nome.value);
+
+    saida.innerText = "nome:"+nome.value+
+    "\n Email: " + email.value;
+   
 }
